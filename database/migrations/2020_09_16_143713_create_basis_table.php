614ed31a35fb2336a3eef0e4c2703dd5ab058d7c
wangshengsheng0215 <1422206528@qq.com>
2020-09-17 10:31:24 +0800

    修改数据库

--- database/migrations/2020_09_16_143713_create_basis_table.php
+++ database/migrations/2020_09_16_143713_create_basis_table.php
@@ -22,10 +22,16 @@ class CreateBasisTable extends Migration
             $table->string('name')->comment('姓名');
             $table->string('classname')->comment('班级');
             $table->string('project')->comment('考核项目名称');
+            $table->string('childproject')->nullable()->comment('考核子项目名称');
+            $table->integer('status')->comment('实验结果');
             $table->integer('grade')->comment('考核成绩');
+            $table->bigInteger('startdate')->comment('实验开始时间');
+            $table->bigInteger('enddate')->comment('实验结束时间');
             $table->integer('hour')->nullable()->comment('考核小时数');
             $table->integer('minute')->nullable()->comment('考核分钟数');
             $table->integer('second')->nullable()->comment('考核秒数');
+            $table->integer('issuserid')->comment('接入平台编号');
+            $table->integer('fileid')->nullable()->comment('文件id');
 
             $table->timestamp('addtime')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('添加时间');
             $table->timestamp('updatetime')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('修改时间');
